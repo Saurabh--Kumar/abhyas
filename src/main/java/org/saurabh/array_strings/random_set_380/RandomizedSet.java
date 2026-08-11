8b6cde76c65d6a6fe1c935c6bc00b1ddaeab5b0a
Saurabh Kumar <bhardwajsaurabh10@gmail.com>
2026-08-11 21:03:42 +0530

    random set

---
+++ src/main/java/org/saurabh/array_strings/random_set_380/RandomizedSet.java
@@ -0,0 +1,44 @@
+package org.saurabh.array_strings.random_set_380;
+
+import java.util.*;
+
+class RandomizedSet {
+
+    private Map<Integer, Integer> randomMap;
+    private List<Integer> randomList;
+    private int size;
+    private Random rand;
+
+    public RandomizedSet() {
+        randomMap = new HashMap<>();
+        randomList = new ArrayList<>();
+        size = 0;
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if(randomMap.containsKey(val)){
+            return false;
+        }
+        randomMap.put(val, size);
+        randomList.add(val);
+        size++;
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if(!randomMap.containsKey(val)){
+            return false;
+        }
+        int randomListIndex = randomMap.get(val);
+        randomList.set(randomListIndex, randomList.get(size-1));
+        randomList.remove(size-1);
+        randomMap.remove(val);
+        size--;
+        return true;
+    }
+
+    public int getRandom() {
+        return randomList.get(rand.nextInt(size));
+    }
+}
