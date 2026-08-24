da2027e4c5e187fafac9169c1113d291400d5de4
Saurabh Kumar <bhardwajsaurabh10@gmail.com>
2026-08-24 20:29:39 +0530

    max water container 11

---
+++ src/main/java/org/saurabh/array_strings/container_water_11/Solution.java
@@ -0,0 +1,20 @@
+package org.saurabh.array_strings.container_water_11;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length-1;
+        int maxVolume = 0;
+        while(start<end){
+            int volume = Math.min(height[start], height[end]) * (end - start);
+            maxVolume = Math.max(maxVolume, volume);
+            if(height[start] < height[end]){
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return maxVolume;
+    }
+}
