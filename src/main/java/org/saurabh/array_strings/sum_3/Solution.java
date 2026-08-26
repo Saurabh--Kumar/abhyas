82f25905af053a9495634563df174a2660ed1b48
Saurabh Kumar <bhardwajsaurabh10@gmail.com>
2026-08-26 16:34:08 +0530

    3_sum 15

---
+++ src/main/java/org/saurabh/array_strings/sum_3/Solution.java
@@ -0,0 +1,50 @@
+package org.saurabh.array_strings.sum_3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> output = new ArrayList<>();
+
+        int i = 0;
+        int j,k;
+
+        while (i< nums.length){
+            j = i+1;
+            k = nums.length -1;
+            while(j<k){
+                if(nums[j] + nums[k] == -1*nums[i]){
+                    output.add(List.of(nums[i], nums[j], nums[k]));
+                    while(j+1 < nums.length && nums[j+1] == nums[j]){
+                        j++;
+                    }
+
+                    while(k-1 > j && nums[k-1] == nums[k]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(nums[j] + nums[k] > -1*nums[i]){
+                    k--;
+                } else{
+                    j++;
+                }
+            }
+            while(i+1 < nums.length && nums[i+1] == nums[i]){
+                i++;
+            }
+            i++;
+
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.threeSum(new int[]{-2,0,1,1,2});
+    }
+}
